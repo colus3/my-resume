0c96f3b58c057b28027f26df310c1983a115c30e
Donghwan Lee <colus4@gmail.com>
2016-08-19 18:38:43 +0900

    add Title

--- client/compoments/app.js
+++ client/compoments/app.js
@@ -2,13 +2,14 @@
  * Created by Colus on 2016. 8. 19..
  */
 import React from 'react';
+import Title from './title';
 
 export default class App extends React.Component {
-
+  
   render() {
     return (
       <div>
-        <h1>Hello React!</h1>
+        <Title/>
         <h3>Example heading <span className="label label-default">New</span></h3>
       </div>
     );
---
+++ client/compoments/title.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Colus on 2016. 8. 19..
+ */
+import React from 'react';
+
+export default class Title extends React.Component {
+  
+  render() {
+    return (
+      <div className="page-header jumbotron" id="content" tabIndex="-1">
+        <div className="container">
+          <h1>Lee Dong Hwan</h1>
+          <h2>Programmer</h2>
+        </div>
+      </div>
+    );
+  }
+}
