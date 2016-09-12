8189a08fed5515fbe6eaa993b5c443576aab6c96
Donghwan Lee <colus4@gmail.com>
2016-09-12 19:06:58 +0900

    주석 추가

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.enable('trust proxy');
 
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
-app.use(express.static(path.join(__dirname, './../public')));
+app.use(express.static(path.join(__dirname, './../public'))); // Static Resource
 app.get('/resume/:id*?', handleRender); // React Server Side Rendering
-app.use('/api', api);
+app.use('/api', api); // Server API
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}`);
