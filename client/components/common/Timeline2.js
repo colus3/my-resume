e609e53c50e3c180071b788106eb9ea31f84f737
Donghwan Lee <colus4@gmail.com>
2016-09-12 19:06:06 +0900

    타임라인 마지막 표시 추가 및 타임라인 생성 로직 함수 분리

--- client/components/common/Timeline2.js
+++ client/components/common/Timeline2.js
@@ -16,43 +16,14 @@ class TimeLine2 extends React.Component {
   }
   
   render() {
-    
-    const style={ 'display': 'inline-block', 'marginRight': '5px', 'color': '#fff' };
+    const timeLines = createTimeLine( this.props.datas, this.props.useYearLabel );
   
-    let yearLebel = '';
-    const timeLines = this.props.datas.reduce((list, data, index) => {
-  
-      let label;
-      if ( this.props.useYearLabel && yearLebel !== data.startDate.substr(0, 4) ) {
-        yearLebel = data.startDate.substr(0, 4);
-        label = (
-          <span key={`timeline-label${index}`} className="timeline-label">
-            <span className="label label-primary">{yearLebel}</span>
-          </span>
-        );
-      }
-      
-      const item = (
-        <div key={data.id} className="timeline-item">
-          <div className="timeline-point timeline-point-blank timeline-point-success"></div>
-          <div className="timeline-event">
-              <div className="timeline-heading">
-                <h4 className="timeline-title">
-                  <span name="title">{data.startDate} ~ {data.endDate} <strong>{data.title}</strong> </span>
-                </h4>
-              </div>
-              <div className="timeline-body">
-              <p>{data.desc === '' ? '' : data.desc}</p>
-              <p>
-                {data.badges === [] ? '' : data.badges.map( (badge, i) => ( <span key={i} className="label label-primary" style={style}>{badge}</span> ) )}
-              </p>
-              </div>
-          </div>
-        </div>
-      );
-      
-      return list.concat([label, item]);
-    }, []);
+    if ( this.props.useYearLabel ) {
+      timeLines.push((
+        <span key="timeline-labelEnd" className="timeline-label">
+          <span className="label label-primary">End</span>
+        </span>));
+    }
   
     return (
       <div className="timeline">
@@ -60,6 +31,51 @@ class TimeLine2 extends React.Component {
       </div>
     );
   }
+}
+
+function createTimeLine( datas, useYearLabel ) {
+  
+  let yearLebel = '';
+  return datas.reduce((list, data, index) => {
+    
+    let label;
+    if ( useYearLabel && yearLebel !== data.startDate.substr(0, 4) ) {
+      yearLebel = data.startDate.substr(0, 4);
+      label = (
+        <span key={`timeline-label${index}`} className="timeline-label">
+            <span className="label label-primary">{yearLebel}</span>
+          </span>
+      );
+    }
+    
+    const item = (
+      <div key={data.id} className="timeline-item">
+        <div className="timeline-point timeline-point-blank timeline-point-success"></div>
+        <div className="timeline-event">
+          <div className="timeline-heading">
+            <h4 className="timeline-title">
+              <span name="title">{data.startDate} ~ {data.endDate} <strong>{data.title}</strong> </span>
+            </h4>
+          </div>
+          <div className="timeline-body">
+            <p>{data.desc === '' ? '' : data.desc}</p>
+            <p>
+              {data.badges === [] ? '' :
+                data.badges.map(
+                  (badge, i) => (
+                    <span key={i} className="label label-primary" style={{'display': 'inline-block', 'marginRight': '5px', 'color': '#fff'}}>
+                      {badge}
+                    </span>
+                  )
+                )}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+    
+    return list.concat([label, item]);
+  }, []);
 }
 
 TimeLine2.propTypes = propTypes;
