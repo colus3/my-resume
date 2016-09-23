b87fad7b5047f5035f8f02e2b5e7be937942e065
Donghwan Lee <colus4@gmail.com>
2016-09-23 18:20:12 +0900

    불필요한 소스 제거

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 import express from 'express';
-// import path from 'path';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
-
-// import { handleRender, renderToString } from '../public/server';
 
 import router from './routes/resume';
 
@@ -16,9 +13,6 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(bodyParser.json()); // for parsing application/json
 app.use('/resume', router);
-// app.use('/', express.static(path.join(__dirname, './../public'))); // Static Resource
-// app.get('/resume/:id*?', handleRender); // React Server Side Rendering
-// app.use('/api', api); // Server API
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}`);
