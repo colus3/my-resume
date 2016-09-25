3919899ae291605005f9c0fb38cedefa1208e551
Donghwan Lee <colus4@gmail.com>
2016-09-25 13:28:16 +0900

    Webpack 설정 변경 : build 할때 uglifyJs Plugin 작동

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = [
       preLoaders: preLoaders,
       loaders: loaders
     },
-/*    plugins: plugins, */
+    plugins: plugins,
     devServer: {
       inline: true,
       port: 7777,
