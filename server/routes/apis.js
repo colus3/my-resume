5b20a2df61df1b01f6bfbe53e3be616519f55009
Donghwan Lee <colus4@gmail.com>
2016-09-25 14:02:53 +0900

    PDF Download URL 변경

--- server/routes/apis.js
+++ server/routes/apis.js
@@ -28,7 +28,7 @@ router.get('/download/:id', function(req, res) {
     
   }).then(page => {
     _page = page;
-    let url = `${req.protocol}://${req.get('host')}/resume/${id ? id : ''}`;
+    let url = `${req.protocol}://${req.get('host')}/resume/id/${id ? id : ''}`;
     return page.open(url);
     
   }).then(status => {
