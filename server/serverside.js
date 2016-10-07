abc33ff1b669d81d0f1ffbf14bf9efe1b810bcaf
Donghwan Lee <colus4@gmail.com>
2016-10-08 08:36:44 +0900

    import 수정 및 잘못된 소스 수정

--- server/serverside.js
+++ server/serverside.js
@@ -7,7 +7,7 @@ import { App } from 'components';
 import resumePage from '../public/resume.html';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { reducer } from 'reducers';
+import reducer from 'reducers';
 
 import { getInitialData } from './db/orientdb';
 
